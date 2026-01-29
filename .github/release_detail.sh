b1c7818b1705c477affbf26134513e3d15eb02ab
Claude Warren <claude.warren@aiven.io>
2026-01-29 12:06:40 +0000

    updates as per review

--- .github/release_detail.sh
+++ .github/release_detail.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2024 Copyright 2022 Aiven Oy and
-# bigquery-connector-for-apache-kafka project contributors
-#
-# This software contains code derived from the Confluent BigQuery
-# Kafka Connector, Copyright Confluent, Inc, which in turn
-# contains code derived from the WePay BigQuery Kafka Connector,
-# Copyright WePay, Inc.
+# Copyright 2026 Aiven Oy and project contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,23 +15,25 @@
 # specific language governing permissions and limitations
 # under the License.
 #
+#        SPDX-License-Identifier: Apache-2
+#
 
 git fetch origin
-if [ -z $1 ]
+if [ -z $2 ]
 then
-  echo "Must provide final version"
+  echo "Must provide source and final version"
   exit 1
 fi
 
-startTag=`grep latestRelease pom.xml | cut -f2 -d">" | cut -f1 -d"<"`
-endTag=${1}
+startTag=${1}
+endTag=${2}
 
-start=`git rev-parse v${startTag}`
+start=`git rev-parse ${startTag}`
 end=`git rev-parse HEAD`
 commits=${start}...${end}
-printf "## v%s\n### What's changed\n" ${endTag}
+printf "## %s\n### What's changed\n" ${endTag}
 git log --format=' - %s'  ${commits}
 
 printf "\n\n### Co-authored by\n"
 git log --format=' - %an'  ${commits} | sort -u
-printf "\n\n### Full Changelog\nhttps://github.com/Aiven-Open/bigquery-connector-for-apache-kafka/compare/v${startTag}...v${endTag}\n\n"
+printf "\n\n### Full Changelog\nhttps://github.com/Aiven-Open/aiven-commons/compare/${startTag}...${endTag}\n\n"
