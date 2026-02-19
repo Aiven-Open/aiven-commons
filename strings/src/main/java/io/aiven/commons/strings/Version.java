86c4a5c4b1ada4298c919c0aefa92697deaefbd2
Claude Warren <claude.warren@aiven.io>
2026-02-19 17:59:29 +0000

    added tests, cleand up comments

--- strings/src/main/java/io/aiven/commons/strings/Version.java
+++ strings/src/main/java/io/aiven/commons/strings/Version.java
@@ -5,13 +5,15 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ *        SPDX-License-Identifier: Apache-2
  */
 
 package io.aiven.commons.strings;
@@ -19,23 +21,21 @@ package io.aiven.commons.strings;
 import java.io.InputStream;
 import java.util.Properties;
 
-
-
 /**
- * Reads a list of versions from a configuration file.
- * Versions may be retrieved by calling {@link #of(String)}.
- * By default, reads versions from "app.properties"
- * <p>
- *     Recommended usage is to add a package version property in the pom.xml that looks something like
- *     <pre>{@code
+ * Reads a list of versions from a configuration file. Versions may be retrieved
+ * by calling {@link #of(String)}. By default, reads versions from
+ * "app.properties" Recommended usage is to add a package version property in
+ * the pom.xml that looks something like
+ * 
+ * <pre>{@code
  *          <properties>
  *              <my-groupid-and-artifactid-version>1.3.4</my-groupid-and-artifactid-version>
  *          </properties>
  *     }</pre>
  *
- *     use the {@code properties-maven-plugin} to generate the properties file.
+ * use the {@code properties-maven-plugin} to generate the properties file.
  *
- *     <pre>{@code
+ * <pre>{@code
  *     <plugin>
  *         <groupId>org.codehaus.mojo</groupId>
  *         <artifactId>properties-maven-plugin</artifactId>
@@ -54,51 +54,62 @@ import java.util.Properties;
  *       </plugin>
  *     }</pre>
  *
- *    and then retrieve the code in the project as
+ * and then retrieve the code in the project as
  *
- *  <pre>{@code
- *      public static String VERSION = new Version("groupid/artifactid.properties").of("my-groupid-and-artifactid-version");
- *  }</pre>
+ * <pre>{@code
+ * public static String VERSION = new Version("groupid/artifactid.properties").of("my-groupid-and-artifactid-version");
+ * }</pre>
  *
- *    If additional values from the properties are desired then creating an instance of {@code Version} and calling
- *    {@link #of(String)} for the desired properties will work.
- * </p>
+ * If additional values from the properties are desired then creating an
+ * instance of {@code Version} and calling {@link #of(String)} for the desired
+ * properties will work.
  */
 public final class Version {
-    private static final String PROPERTIES_FILENAME = "app.properties";
+	private static final String PROPERTIES_FILENAME = "app.properties";
+	/** The recommended property name for the project.version */
+	public static final String RECOMMENDED_PROPERTY = "project-version";
+	private final Properties versions;
+	private final String errorMsg;
 
-    private final Properties versions;
-    private final String errorMsg;
+	/**
+	 * Default constructor. Reads "app.properties"
+	 */
+	public Version() {
+		this(PROPERTIES_FILENAME);
+	}
 
+	/**
+	 * property loader. Reads properties from the specified file which must be in
+	 * the classpath.
+	 * 
+	 * @param propertyFileName
+	 *            the name of the property file to read
+	 */
+	public Version(String propertyFileName) {
+		versions = new Properties();
+		String errMsg = null;
+		try (InputStream resourceStream = Thread.currentThread().getContextClassLoader()
+				.getResourceAsStream(propertyFileName)) {
+			versions.load(resourceStream);
+		} catch (final Exception e) { // NOPMD AvoidCatchingGenericException
+			errMsg = String.format("Error while loading %s: %s", propertyFileName, e.getMessage());
+		} finally {
+			errorMsg = errMsg;
+		}
+	}
 
-    /**
-     * Default constructor.  Reads "app.properties"
-     */
-    public Version() {
-        this(PROPERTIES_FILENAME);
-    }
-
-    /**
-     * property loader.  Reads properties from the specified file which must be in the classpath.
-     * @param propertyFileName
-     */
-    public Version(String propertyFileName) {
-        versions = new Properties();
-        String errMsg = null;
-        try (InputStream resourceStream = Thread.currentThread().getContextClassLoader()
-                .getResourceAsStream(propertyFileName)) {
-            versions.load(resourceStream);
-        } catch (final Exception e) { // NOPMD AvoidCatchingGenericException
-            errMsg = String.format("Error while loading %s: %s", propertyFileName, e.getMessage());
-        } finally {
-            errorMsg = errMsg;
-        }
-    }
-
-    public String of(String property) {
-        if (errorMsg != null) {
-            return errorMsg;
-        }
-        return versions.getProperty(property, "unknown");
-    }
+	/**
+	 * Retrieve a string for the property as defined in the property file.
+	 * 
+	 * @param property
+	 *            the name of the property.
+	 * @return the property stirng, "unknown" if not set, or an error message if the
+	 *         file was not read.
+	 */
+	public String of(String property) {
+		if (errorMsg != null) {
+			return errorMsg;
+		}
+		return versions.getProperty(property, "unknown");
+	}
 }
